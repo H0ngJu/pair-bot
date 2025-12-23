7c7a4834101bdc512e2ebdf58ebbaf0e526e6b91
H0ngJu <hongju4385@gmail.com>
2025-12-23 18:37:50 +0900

    feat: google sheet 연동

---
+++ googleSheets.js
@@ -0,0 +1,30 @@
+import { google } from "googleapis";
+import dotenv from "dotenv";
+dotenv.config();
+
+const auth = new google.auth.GoogleAuth({
+  keyFile: "service-account.json",
+  scopes: ["https://www.googleapis.com/auth/spreadsheets"],
+});
+
+const sheets = google.sheets({ version: "v4", auth });
+
+const SPREADSHEET_ID = process.env.SPREADSHEET_ID;
+console.log("SPREADSHEET_ID =", process.env.SPREADSHEET_ID);
+
+export async function appendRow(sheetName, values) {
+  await sheets.spreadsheets.values.append({
+    spreadsheetId: SPREADSHEET_ID,
+    range: `${sheetName}!A1`,
+    valueInputOption: "USER_ENTERED",
+    requestBody: { values: [values] },
+  });
+}
+
+export async function getRows(sheetName) {
+  const res = await sheets.spreadsheets.values.get({
+    spreadsheetId: SPREADSHEET_ID,
+    range: sheetName,
+  });
+  return res.data.values.slice(1);
+}
